40be00895909fdf1f6f96f7efb0e1d6056aac617
woodensRangerWZJ <13029768866@163.com>
2022-04-18 17:47:54 +0800

    refactor(root): refactor init

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { wrapperEnv } from './build'
+
+// 进程的当前目录
+const root = process.cwd();
+
+// 路径查找
+const pathResolve = dir => {
+  return resolve(__dirname, ".", dir)
+}
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()]
+export default defineConfig(({ command, mode}) => {
+  return {
+    resolve: {
+      /* 别名 */
+      alias: {
+        "/@": pathResolve("src")
+      }
+    },
+    plugins: [vue()]
+  }
 })
