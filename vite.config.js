f949d96282512f78da2559148386f0eaab5a597a
woodensRangerWZJ <35355447+13029768866@users.noreply.github.com>
2022-04-18 22:39:29 +0800

    feat(viteconifg): viteconfig base init

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { resolve } from 'path'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { wrapperEnv } from './build'
+
 
 // 进程的当前目录
 const root = process.cwd();
@@ -12,14 +13,52 @@ const pathResolve = dir => {
 }
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command, mode}) => {
+export default defineConfig(({command, mode}) => {
+  /* 情景模式配置: command代表指令(dev/serve/build) */
+  /* loadEnv根据root(当前目录)加载环境变量文件(.env) */
+  const {
+    VITE_PORT,
+    VITE_LEGACY,
+    VITE_PUBLIC_PATH,
+    VITE_PROXY_DOMAIN,
+    VITE_PROXY_DOMAIN_REAL
+  } = wrapperEnv(loadEnv(mode, root));
+
   return {
+    // 项目根目录（index.html 文件所在的位置）,默认process.cwd()
+    root,
+    // 公共基础路径
+    base: VITE_PUBLIC_PATH,
+    // 文件导入配置
     resolve: {
-      /* 别名 */
+      // 别名配置
       alias: {
         "/@": pathResolve("src")
       }
     },
-    plugins: [vue()]
+    /* css todo list */
+    // 服务器选项
+    server: {
+      host: "0.0.0.0",
+      port: VITE_PORT,
+      https: false,
+      open: true,
+      // 本地跨域代理（todo）
+
+    },
+    // 构建选项
+    build: {
+      // vite在build时候默认进行压缩计算,但是vite并不提供gzip压缩功能,关闭后可以跳过计算压缩大小,提高打包速度
+      brotliSize: false,
+      // 消除打包大小500kb警告
+      chunkSizeWarningLimit: 2000
+    },
+    // 插件配置（todo 单独维护）
+    plugins: [vue()],
+    // 依赖优化选项
+    optimizeDeps: {
+      // 默认情况下,不在node_modules中的链接包不会被预构建,可强制预构建想要的链接包
+      include: []
+    }
   }
-})
+});
